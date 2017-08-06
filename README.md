2aad6a3457678ab7bb28407dbed1e6199336e42a
ExSoax <ciafrei@matematici.com>
2017-08-06 18:01:51 +0200

    Initial Commit
    Version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # jQuery-UI-Dialog-Custom-Titlebar
 Extension for jQuery UI for making windows-like dialog
+
+## Compatibility
+1. jQuery UI v1.12.1
+1. jQuery 1.7+
+
+## General
+This extension is designed for adding custom buttons to jQuery's dialogs titlebar.
+Just add a reference to the script 'jquery.ui.dialog.js', after the place where is located the jQueryUI library in your HTML page.
+
+## Usage
+
+```
+$("<div />")
+  .dialog({ autoOpen: true })
+  .dialogExtend({
+    buttons: [
+      {
+        icon: "ui-icon-newwin",
+        tooltip: "New Window",
+        click: function(event){
+          console.log(event);
+        }
+      },
+      {
+        icon: "ui-icon-newwin",
+        click: function(event){
+          console.log(event);
+        }
+      }
+    ]
+  });
+```
+
+Pass buttons as option for the 'dialogExtend' call.
+Every button has icon, tooltip and click properties.
+
+1. icon : string that points to jQuery UI icons set.
+1. tooltip: string that will be shown as tooltip on the button.
+1. click: callback on button's click event.
---
+++ jquery.ui.dialog.js
@@ -0,0 +1,57 @@
+(function() {
+  var $;
+
+  $ = jQuery;
+
+  $.widget("ui.dialogExtend", {
+    version: "1.0.0",
+    modes: {},
+    options: {
+      "buttons": []
+    },
+    _initStyles: function(){
+      if (!$(".dialog-extend-css").length) {
+        style = '';
+        style += '<style class="dialog-extend-css" type="text/css">';
+        style += ".ui-dialog .ui-dialog-titlebar-genericButton {";
+        style += "position: absolute;"
+        style += "right: .3em;"
+        style += "top: 50%;"
+        style += "width: 20px;"
+        style += "margin: -10px 0 0 0;"
+        style += "padding: 1px;"
+        style += "height: 20px;"
+        style += "}";
+        style += '</style>';
+        $(style).appendTo("body");
+      }
+    },
+    _create: function() {
+      titlebar = $(this.element[0]).dialog("widget").find(".ui-dialog-titlebar");
+      this._initStyles();
+      let i = 0;
+      for (let currentButton of this.options.buttons) {
+        var button =
+          $("<button type='button'></button>")
+          .button({
+            label: $("<a>").text(currentButton.tooltip).html(),
+            icon: "ui-icon-newwin",
+            showLabel: false
+          })
+          .css("margin-right", (25+i*25)+"px")
+          .appendTo(titlebar);
+
+        this._addClass(button, "ui-dialog-titlebar-genericButton");
+        this._on(button, {
+          click: function(event) {
+            event.preventDefault();
+            if (button.click != undefined)
+              button.click(event);
+          }
+        });
+        i++;
+      }
+    }
+  });
+
+}).call(this);
